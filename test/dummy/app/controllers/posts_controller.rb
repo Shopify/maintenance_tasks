7e0ffe6f961a5f86825eacd03084f121c28eca58
Adrianna Chang <adrianna.chang@shopify.com>
2020-09-28 13:12:46 -0400

    Remove scaffolding comments and uncessary helper file

--- test/dummy/app/controllers/posts_controller.rb
+++ test/dummy/app/controllers/posts_controller.rb
@@ -2,25 +2,20 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
   def create
     @post = Post.new(post_params)
 
@@ -31,7 +26,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
       redirect_to(@post, notice: 'Post was successfully updated.')
@@ -40,7 +34,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
   def destroy
     @post.destroy
     redirect_to(posts_url, notice: 'Post was successfully destroyed.')
@@ -48,12 +41,10 @@ class PostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def post_params
     params.fetch(:post, {})
   end
