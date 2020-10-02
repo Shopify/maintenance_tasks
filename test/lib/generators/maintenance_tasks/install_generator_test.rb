9eba6eb2ec674582102893857cc81b4dfab414bc
Adrianna Chang <adrianna.chang@shopify.com>
2020-10-02 11:48:19 -0400

    Implement InstallGenerator for setting up engine in host applications

---
+++ test/lib/generators/maintenance_tasks/install_generator_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+require 'test_helper'
+require 'generators/maintenance_tasks/install_generator'
+
+module MaintenanceTasks
+  class InstallGeneratorTest < Rails::Generators::TestCase
+    tests InstallGenerator
+    SAMPLE_APP_PATH = Engine.root.join('tmp/sample_app')
+    destination SAMPLE_APP_PATH
+    setup :prepare_destination
+
+    def setup
+      super
+      setup_sample_app
+    end
+
+    def teardown
+      FileUtils.rm_rf(SAMPLE_APP_PATH)
+    end
+
+    test 'generator mounts engine, runs migrations, and creates required files' do
+      Dir.chdir(SAMPLE_APP_PATH) do
+        run_generator
+
+        assert_file('config/routes.rb') do |contents|
+          assert_match(
+            %r{mount MaintenanceTasks::Engine => '/maintenance_tasks'},
+            contents
+          )
+        end
+
+        mig = 'db/migrate/create_maintenance_tasks_runs.maintenance_tasks.rb'
+        assert_migration(mig)
+        assert_file('db/schema.rb') do |contents|
+          assert_match(/create_table "maintenance_tasks_runs"/, contents)
+        end
+
+        assert_file('app/jobs/maintenance/application_task.rb')
+        assert_file('config/initializers/maintenance_tasks.rb')
+      end
+    end
+
+    private
+
+    def setup_sample_app
+      FileUtils.copy_entry(Rails.root, SAMPLE_APP_PATH)
+
+      Dir.chdir(SAMPLE_APP_PATH) do
+        FileUtils.rm('config/initializers/maintenance_tasks.rb')
+        FileUtils.rm('app/jobs/maintenance/application_task.rb')
+
+        FileUtils.rm('db/schema.rb')
+        %x(rails db:drop)
+      end
+    end
+  end
+end
