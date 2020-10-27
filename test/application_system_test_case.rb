70108496a2088c9766797110cc79e17e0e0346d8
Volmer Campos Soares <git@radicaos.com>
2020-10-27 12:58:21 -0400

    Use Selenium with headless Chrome in system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'webdrivers/chromedriver'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include ActiveJob::TestHelper
 
-  driven_by :rack_test
+  driven_by :selenium, using: :headless_chrome do |options|
+    options.add_argument('--disable-dev-shm-usage')
+  end
 
   setup do
     travel_to Time.zone.local(2020, 01, 01, 01, 00, 00)
     Maintenance::UpdatePostsTask.fast_task = false
   end
+
   teardown { Maintenance::UpdatePostsTask.fast_task = true }
 end
