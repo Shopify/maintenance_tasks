94884ef62f26ebde4717d9cc34ef7065d5ddfc01
Adrianna Chang <adrianna.chang@shopify.com>
2020-10-29 15:27:08 -0400

    Use railtie to set up host app when classic autoloader in use

--- lib/maintenance_tasks/engine.rb
+++ lib/maintenance_tasks/engine.rb
@@ -4,5 +4,24 @@ module MaintenanceTasks
   # by the host application.
   class Engine < ::Rails::Engine
     isolate_namespace MaintenanceTasks
+
+    config.to_prepare do
+      unless Rails.autoloaders.zeitwerk_enabled?
+        begin
+          tasks_module = MaintenanceTasks.tasks_module.name.underscore
+        rescue NameError
+          nil
+        end
+        if tasks_module
+          Dir["#{Rails.root}/app/tasks/#{tasks_module}/*.rb"].each do |file|
+            require_dependency(file)
+          end
+        end
+      end
+    end
+
+    config.after_initialize do
+      eager_load! unless Rails.autoloaders.zeitwerk_enabled?
+    end
   end
 end
--- test/dummy/config/initializers/maintenance_tasks.rb
+++ test/dummy/config/initializers/maintenance_tasks.rb
@@ -2,12 +2,3 @@
 
 # Custom configuration for the tasks namespace can be defined here, ie.
 # MaintenanceTasks.tasks_module = 'Maintenance'
-
-unless Rails.autoloaders.zeitwerk_enabled?
-  MaintenanceTasks::Engine.eager_load!
-  Rails.application.config.to_prepare do
-    Dir["#{Rails.root}/app/tasks/maintenance/*.rb"].each do |file|
-      require_dependency(file)
-    end
-  end
-end
