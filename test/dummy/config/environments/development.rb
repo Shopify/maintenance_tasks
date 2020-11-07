4c82f5536dca820e9d1ab820bdd1a597dc9a5071
Étienne Barrié <etienne.barrie@shopify.com>
2020-11-06 20:49:53 -0500

    Remove development environment config for tests
    These were added because the tests were running in the development
    environment.

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -62,7 +62,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Whitelist host used in integration tests.
-  config.hosts << 'www.example.com'
 end
