76b7ee27a37d714b826238b14a44beb82f8ca92c
Volmer Campos Soares <git@radicaos.com>
2020-11-16 14:21:34 -0500

    Executable to run Maintenance Tasks

---
+++ lib/maintenance_tasks/cli.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'thor'
+
+module MaintenanceTasks
+  # Defines the command line interface commands exposed by Maintenance Tasks in
+  # the executable file.
+  class CLI < Thor
+    class << self
+      # Return a failed exit status in case of an error.
+      def exit_on_failure?
+        true
+      end
+    end
+
+    desc 'perform [TASK NAME]', 'Runs the given Maintenance Task'
+
+    long_desc <<-LONGDESC
+      `maintenance_tasks perform` will run the Mainteance Task specified by the
+      [TASK NAME] argument.
+
+      Available Tasks:
+
+      #{MaintenanceTasks::Task.available_tasks.join("\n\n")}
+    LONGDESC
+
+    # Command to run a Task.
+    #
+    # It instantiates a Runner and sends a run message with the given Task name.
+    #
+    # @param name [String] the name of the Task to be run.
+    def perform(name)
+      task = Runner.new.run(name: name)
+      say_status(:success, "#{task.name} was enqueued.", :green)
+    rescue => error
+      say_status(:error, error.message, :red)
+    end
+  end
+  private_constant :CLI
+end
---
+++ test/lib/maintenance_tasks/cli_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'maintenance_tasks/cli'
+
+module MaintenanceTasks
+  class CLITest < ActiveSupport::TestCase
+    setup do
+      @cli = CLI.new
+    end
+
+    test '.exit_on_failure? is true' do
+      assert_predicate CLI, :exit_on_failure?
+    end
+
+    test '#perfom runs the given Task and prints a success message' do
+      task = mock(name: 'MyTask')
+
+      Runner.any_instance.expects(:run).with(name: 'MyTask').returns(task)
+      @cli.expects(:say_status).with(:success, 'MyTask was enqueued.', :green)
+
+      @cli.perform('MyTask')
+    end
+
+    test '#perfom prints an error message when the runner raises' do
+      Runner.any_instance.expects(:run).with(name: 'Wrong').raises('Invalid!')
+      @cli.expects(:say_status).with(:error, 'Invalid!', :red)
+
+      @cli.perform('Wrong')
+    end
+  end
+end
