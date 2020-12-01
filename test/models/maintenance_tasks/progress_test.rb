0869d9a5bf67f400ae1fd3402cb708be94b87d65
Volmer Campos Soares <git@radicaos.com>
2020-12-01 08:49:44 -0500

    Refactor progress view helper with a new Progress class

---
+++ test/models/maintenance_tasks/progress_test.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module MaintenanceTasks
+  class ProgressTest < ActiveSupport::TestCase
+    setup do
+      @run = Run.new(tick_total: 7, tick_count: 4, status: :running)
+      @progress = Progress.new(@run)
+    end
+
+    test '#value is the Run tick count' do
+      assert_equal 4, @progress.value
+    end
+
+    test '#value is nil if the Run does not have a tick total' do
+      @run.tick_total = nil
+      assert_nil @progress.value
+    end
+
+    test '#value is the Run tick count if the Run does not have a tick total and it is stopped' do
+      @run.status = :paused
+      @run.tick_total = nil
+
+      assert_equal 4, @progress.value
+    end
+
+    test '#max is the Run tick total' do
+      assert_equal 7, @progress.max
+    end
+
+    test '#max is the Run tick count if the Run does not have a tick total' do
+      @run.tick_total = nil
+      assert_equal 4, @progress.max
+    end
+
+    test '#title returns a description with tick count, tick total, and percentage' do
+      assert_equal 'Processed 4 out of 7 (57%)', @progress.title
+    end
+
+    test '#title returns a description with tick count when tick total is not present' do
+      @run.tick_total = nil
+      assert_equal 'Processed 4 items.', @progress.title
+    end
+
+    test '#title pluralizes the description according to the tick count' do
+      @run.tick_count = 1
+      @run.tick_total = nil
+      assert_equal 'Processed 1 item.', @progress.title
+    end
+  end
+end
