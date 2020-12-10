a9c3c38c20dc34c6ba2cf8e4c3b657c49af38579
Adrianna Chang <adrianna.chang@shopify.com>
2020-12-10 14:06:55 -0500

    Remove unnecessary timestamp indexes and add index to :task_name

--- db/migrate/20201211151756_create_maintenance_tasks_runs.rb
+++ db/migrate/20201211151756_create_maintenance_tasks_runs.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class CreateMaintenanceTasksRuns < ActiveRecord::Migration[6.0]
   def change
-    create_table(:maintenance_tasks_runs) do |t|
+    create_table(:maintenance_tasks_runs, force: true) do |t|
       t.string(:task_name, null: false)
       t.datetime(:started_at)
       t.datetime(:ended_at)
@@ -14,11 +14,9 @@ class CreateMaintenanceTasksRuns < ActiveRecord::Migration[6.0]
       t.string(:error_class)
       t.string(:error_message)
       t.text(:backtrace)
-
       t.timestamps
     end
     add_index(:maintenance_tasks_runs, :created_at)
-    add_index(:maintenance_tasks_runs, :started_at)
-    add_index(:maintenance_tasks_runs, :ended_at)
+    add_index(:maintenance_tasks_runs, :task_name)
   end
 end
