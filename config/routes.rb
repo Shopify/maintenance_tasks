040747989d51a299a7ed9293e1f071ca3dd9738b
Volmer Campos Soares <git@radicaos.com>
2021-01-06 15:58:21 -0500

    Revert "Merge pull request #274 from Shopify/runs-index"
    This reverts commit 9b3f0e1c3a3c7b8f4cbcc5111331134a85b0b526, reversing
    changes made to fc7b8ba86ba27527d2c8464571b54f571f9157bd.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 MaintenanceTasks::Engine.routes.draw do
-  resources :runs, only: [:index], format: false
-
   resources :tasks, only: [:index, :show], format: false do
     member do
       put 'run'
