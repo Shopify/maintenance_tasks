1fdf89a617a4108ea33afb8da7e90e0eb09f1907
Volmer Campos Soares <git@radicaos.com>
2021-01-07 09:51:40 -0500

    Remove needless tests

--- test/lib/maintenance_tasks_test.rb
+++ test/lib/maintenance_tasks_test.rb
@@ -2,19 +2,6 @@
 require 'test_helper'
 
 class MaintenanceTasksTest < ActiveSupport::TestCase
-  test '.tasks_module defaults to constant Maintenance' do
-    assert_equal('Maintenance', MaintenanceTasks.tasks_module)
-  end
-
-  test '.tasks_module can be set' do
-    previous_task_module = MaintenanceTasks.tasks_module
-
-    MaintenanceTasks.tasks_module = 'Task'
-    assert_equal('Task', MaintenanceTasks.tasks_module)
-  ensure
-    MaintenanceTasks.tasks_module = previous_task_module
-  end
-
   test '.configure_bugsnag_integration keeps original error handler if no Bugsnag' do
     original_error_handler = MaintenanceTasks.error_handler
     MaintenanceTasks.configure_bugsnag_integration
