526c5be027056be52374e5867127d771ce47b2fa
Étienne Barrié <etienne.barrie@shopify.com>
2021-01-19 13:10:17 -0500

    form_with are local by default in 6.1

--- test/dummy/config/initializers/new_framework_defaults_6_1.rb
+++ test/dummy/config/initializers/new_framework_defaults_6_1.rb
@@ -46,7 +46,7 @@ Rails.application.config.action_dispatch.ssl_default_redirect_status = 308
 Rails.application.config.active_record.legacy_connection_handling = false
 
 # Make `form_with` generate non-remote forms by default.
-# Rails.application.config.action_view.form_with_generates_remote_forms = false
+Rails.application.config.action_view.form_with_generates_remote_forms = false
 
 # Set the default queue name for the analysis job to the queue adapter default.
 Rails.application.config.active_storage.queues.analysis = nil
