93fadfd4a3ff32f218c2b1ee62897730e1dd3aa7
Sam Bostock <sam.bostock@shopify.com>
2021-01-21 21:44:20 -0500

    Revert "Add dynamic collection parameter introspection"
    This reverts commit 40ea96dc4ff402dee1ebe73613bea4adbf400d21.

--- test/jobs/maintenance_tasks/task_job_test.rb
+++ test/jobs/maintenance_tasks/task_job_test.rb
@@ -268,29 +268,7 @@ module MaintenanceTasks
       assert_predicate @run, :errored?
       assert_equal 'ArgumentError', @run.error_class
       assert_empty @run.backtrace
-      expected_message = 'Maintenance::DynamicTask#collection '\
-        'must be either an Active Record Relation, an Array, a CSV, or an ' \
-        'object responding to .call(cursor:).'
-      assert_equal expected_message, @run.error_message
-    end
-
-    test '.perform_now rejects improperly callable object collection' do
-      @run = Run.create!(task_name: 'Maintenance::DynamicTask')
-
-      Maintenance::DynamicTask.any_instance.stubs(
-        collection: CallableMissingCursorKeyword.new,
-      )
-      Maintenance::DynamicTask.any_instance.expects(:process).never
-
-      TaskJob.perform_now(@run)
-      @run.reload
-
-      assert_predicate @run, :errored?
-      assert_equal 'ArgumentError', @run.error_class
-      assert_empty @run.backtrace
-      expected_message = 'Maintenance::DynamicTask#collection '\
-        'must be either an Active Record Relation, an Array, a CSV, or an ' \
-        'object responding to .call(cursor:).'
+      expected_message = 'wrong number of arguments (given 1, expected 0)'
       assert_equal expected_message, @run.error_message
     end
 
@@ -326,12 +304,5 @@ module MaintenanceTasks
     ensure
       MaintenanceTasks.error_handler = error_handler_before
     end
-
-    class CallableMissingCursorKeyword
-      def call
-        raise 'should not be called; missing cursor: keyword parameter!'
-      end
-    end
-    private_constant :CallableMissingCursorKeyword
   end
 end
