9bc2a2d6803660ad615e64d339531ebca1e1f28d
Rafael Mendonça França <rafael.franca@shopify.com>
2021-02-23 19:14:25 +0000

    Expose MaintenanceTasks::ApplicationRecord to public API
    This make possible for apps to configure where the models
    related to this engine will live.

--- app/models/maintenance_tasks/application_record.rb
+++ app/models/maintenance_tasks/application_record.rb
@@ -2,7 +2,8 @@
 module MaintenanceTasks
   # Base class for all records used by this engine.
   #
-  # @api private
+  # Can be extended to setup different database where all tables related to
+  # maintenance tasks will live.
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
   end
