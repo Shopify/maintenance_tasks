4784b42003fc5ec7333c8b46a8c5be33486cd1ae
Étienne Barrié <etienne.barrie@shopify.com>
2021-04-30 10:07:50 -0400

    Force loading of TaskJob with initializer

--- lib/maintenance_tasks.rb
+++ lib/maintenance_tasks.rb
@@ -7,10 +7,6 @@ require "active_record"
 require "job-iteration"
 require "maintenance_tasks/engine"
 
-# Force the TaskJob class to load so we can verify upstream compatibility with
-# the JobIteration gem
-require_relative "../app/jobs/maintenance_tasks/task_job"
-
 # The engine's namespace module. It provides isolation between the host
 # application's code and the engine-specific code. Top-level engine constants
 # and variables are defined under this module.
