e5c6350366c7dedf299972f50c63d52ddd585b30
Étienne Barrié <etienne.barrie@gmail.com>
2021-05-31 10:16:56 -0400

    Fix schema for MySQL
    SQLite dumps bigint columns as integer columns in the database schema,
    even if they should really be bigints for other adapters because they're
    the primary keys of other tables.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_131953) do
     t.string "name", null: false
     t.string "record_type", null: false
     t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_131953) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
