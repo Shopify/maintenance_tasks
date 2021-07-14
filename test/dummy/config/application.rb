547889d7edf30c6b407258d0f1f8d0d9c7115e4e
Adrianna Chang <adrianna.chang@shopify.com>
2021-07-14 11:15:07 -0400

    Revert use of #destroy_all_in_batches

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -33,9 +33,5 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    if Rails::VERSION::STRING.match?(/^7.0/)
-      config.active_record.destroy_all_in_batches = true
-    end
   end
 end
