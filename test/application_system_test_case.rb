581a7a2e8e8c023ef12d7fb638d2e08b3819f8d2
Étienne Barrié <etienne.barrie@shopify.com>
2023-01-25 16:34:43 +0100

    Remove silenced warning by requiring necessary file
    Selenium::WebDriver::Remote::COMMANDS does not exist but instead it's
    Selenium::WebDriver::Remote::Bridge::COMMANDS, defined in
    selenium/webdriver/remote/commands
    
    https://github.com/SeleniumHQ/selenium/blob/selenium-4.7.1-ruby/rb/lib/selenium/webdriver/remote.rb#L30

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,7 @@
 
 require "test_helper"
 require "webdrivers/chromedriver"
+require "selenium/webdriver/remote/commands"
 require "action_dispatch/system_testing/server"
 
 ActionDispatch::SystemTesting::Server.silence_puma = true
