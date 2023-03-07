c8d944be734287bc3f66f0da95faa3e8d1798fb3
Szymon Lipka <szymonlipkaa@gmail.com>
2023-03-07 17:34:38 +0100

    lint

--- app/jobs/concerns/maintenance_tasks/task_job_concern.rb
+++ app/jobs/concerns/maintenance_tasks/task_job_concern.rb
@@ -177,7 +177,7 @@ module MaintenanceTasks
       end
       errored_element = @errored_element if defined?(@errored_element)
     ensure
-      instance_exec(*[error, task_context, errored_element], &MaintenanceTasks.error_handler)
+      instance_exec(error, task_context, errored_element, &MaintenanceTasks.error_handler)
     end
   end
 end
