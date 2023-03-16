a8d289f44526afec09270bb0c4602cff6426d758
Étienne Barrié <etienne.barrie@shopify.com>
2023-03-16 15:27:31 +0100

    Handle Rails 7.1 deprecation of TestFixtures#fixture_path (#790)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,12 +24,15 @@ end
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
+  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
+  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
+  ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
+  ActiveSupport::TestCase.fixtures(:all)
+elsif ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path =
-    ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path =
-    ActiveSupport::TestCase.fixture_path + "/files"
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures(:all)
 end
 
