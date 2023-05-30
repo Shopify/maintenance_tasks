5d3294c040a7365163086e0af56200ebc3f48d51
Étienne Barrié <etienne.barrie@shopify.com>
2023-05-30 15:40:04 +0200

    Don't generate a nonce (#829)
    The need for a nonce was removed in
    3ba21de59b76b60335988eff2b08db0c51d542d6

--- app/controllers/maintenance_tasks/application_controller.rb
+++ app/controllers/maintenance_tasks/application_controller.rb
@@ -20,11 +20,6 @@ module MaintenanceTasks
       policy.frame_ancestors(:self)
     end
 
-    before_action do
-      request.content_security_policy_nonce_generator ||= ->(_request) { SecureRandom.base64(16) }
-      request.content_security_policy_nonce_directives = ["style-src"]
-    end
-
     protect_from_forgery with: :exception
   end
 end
