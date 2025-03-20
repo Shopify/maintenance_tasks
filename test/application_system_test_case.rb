21053665ec17e08be66f176b0322c970971071ab
Étienne Barrié <etienne.barrie@shopify.com>
2025-03-20 16:08:54 +0100

    Don't check for the visibility of nodes
    This causes flakiness when we're navigating pages because Capybara goes
    back to the browser to check for the visibility of the node, which
    doesn't exist anymore.
    
    Co-authored-by: Adrianna Chang <adrianna.chang@shopify.com>

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -11,6 +11,14 @@ ActionDispatch::SystemTesting::Server.silence_puma = true
 # warning in Ruby 2.7.
 Capybara::Selenium::Driver.load_selenium
 
+Capybara.configure do |config|
+  # This causes flakiness when we're navigating pages because Capybara goes back to the browser to check for the
+  # visibility of the node, which doesn't exist anymore:
+  #   Selenium::WebDriver::Error::UnknownError: unknown error: unhandled inspector error:
+  #   {"code":-32000,"message":"Node with given id does not belong to the document"}
+  config.ignore_hidden_elements = false
+end
+
 if Rails.gem_version < Gem::Version.new("7.1")
   Selenium::WebDriver.logger.ignore(:capabilities)
 end
