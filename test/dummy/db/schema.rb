1a213324b2b895e5f4127eb772c2c9366a61e974
Nicholas La Roux <larouxn@gmail.com>
2026-05-15 21:37:07 +0200

    Drop support for no-longer-supported Rails 7.1 and clean up ✨ (#1475)

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_19_151430) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_19_151430) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
